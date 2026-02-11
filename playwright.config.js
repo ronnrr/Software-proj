e2afe09f6439ee6b81de77190a38e7d9fff22335
Safwan Halabi <safwanhalabi60@gmail.com>
2026-02-11 19:59:43 +0200

    removed files, trying again from scratch

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-module.exports = defineConfig({
-testDir: './tests',
-timeout: 30000,
-use: {
-headless: true,
-viewport: { width: 1280, height: 720 }
-}
-});
